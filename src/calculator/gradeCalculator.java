3a6a27b9dd61e3ee84bd58fc8b0dbfeef8e7fb2a
TomGoon <tgoon@wustl.edu>
2022-04-05 15:01:21 -0500

    deleted tests and cleaned main

--- src/calculator/gradeCalculator.java
+++ src/calculator/gradeCalculator.java
@@ -9,88 +9,8 @@ import java.util.*;
 public class gradeCalculator {
 
 	public static void main(String[] args) {
-
-		//file paths are hard coded for now
-		String classWeightFilePath = "class/class_weight.txt";
-		String classGradeFilePath = "grades/class_grade.txt";
-		
-		ArrayList<String> weightsList = readFile(classWeightFilePath);
-		ArrayList<String> gradesList = readFile(classGradeFilePath);
-
-		Hashtable<String, Integer> gradingScale = createGradingScale(weightsList);
-		Hashtable<String, ArrayList<Integer>> grades = createGrades(gradesList,gradingScale);
-		
-		float finalGrade = calculateFinalGrade(grades,gradingScale);
-		System.out.println(finalGrade);
+		System.out.println("This is grade calculator");
 	}
  
-	public static ArrayList<String> readFile(String filePath) {
-				
-		File file = new File("./src/" + filePath);
-		
-		ArrayList<String> fileContents = new ArrayList<String>();
-		
-		try  {
-			Scanner fileIn = new Scanner(file);
-			
-			while(fileIn.hasNextLine()) {
-				String line = fileIn.nextLine();
-				fileContents.add(line);
-			}
-			
-		} catch(FileNotFoundException exception) {
-			exception.printStackTrace();
-		}
-		
-		return fileContents;
-	}
-	
-	public static Hashtable<String, Integer> createGradingScale(ArrayList<String> fileContents) {
-		Hashtable<String, Integer> gradingScale = new Hashtable<String,Integer>();
-		for (String line : fileContents) {
-			//splitting each line into its 2 separate words; first words (grade type) is they key, second word (grade score) is the value
-			String[] words = line.split(" ",2);
-			gradingScale.put(words[0], Integer.parseInt(words[1]));
-		}
-		return gradingScale;
-	}
-	
-	public static Hashtable<String, ArrayList<Integer>> createGrades(ArrayList<String> fileContents, Hashtable<String, Integer> gradingScale) {
-		Hashtable<String, ArrayList<Integer>> grades = new Hashtable<String, ArrayList<Integer>>();
-		Enumeration<String> keys = gradingScale.keys();
-		while (keys.hasMoreElements()) {
-            grades.put(keys.nextElement(),new ArrayList<Integer>());
-        }
-		for (String line : fileContents) {
-			//splitting each line into its 3 separate words; first word (assignment) is ignored, second word (grade type) is the key, third word (weight) is the value
-			String[] words = line.split(" ",3);
-			ArrayList<Integer> currentGrades = grades.get(words[1]);
-			currentGrades.add(Integer.parseInt(words[2]));
-			grades.put(words[1], currentGrades);
-		}
-		return grades;
-	}
-	
-	public static float calculateFinalGrade(Hashtable<String, ArrayList<Integer>> grades, Hashtable<String, Integer> gradingScale) {
-		
-		float finalGrade = 0;
-		
-		Enumeration<String> gradeTypes = grades.keys();
-		while (gradeTypes.hasMoreElements()) {
-			String gradeType = gradeTypes.nextElement();
-			finalGrade += gradingScale.get(gradeType)*averageArrayList(grades.get(gradeType));
-        }
-	
-		return finalGrade / 100;
-	}
-	
-	public static float averageArrayList(ArrayList<Integer> list) {
-	    float sum = 0;
-	    float size = list.size();
-	    for (int i: list) {
-	        sum += i;
-	    }
-	    return sum/size;
-	}
 	
 }
